2bec2bed3173e333c2d42f45a5a265eac3d99674
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-07-11 14:23:27 -0300

    Inserting the recommendations of the PEP8

--- day1/floor.py
+++ day1/floor.py
@@ -1,23 +1,23 @@
 class Floor:
-	@staticmethod
-	def calc_position(self, value):
-		time_up = 0
-		time_down = 0
-		count = 0
-		while count < len(value):
-			if value[count] == '(':
-				time_up += 1
-			elif value[count] == ')':
-				time_down += 1
+    @staticmethod
+    def calc_position(self, value):
+        time_up = 0
+        time_down = 0
+        count = 0
+        while count < len(value):
+            if value[count] == '(':
+                time_up += 1
+            elif value[count] == ')':
+                time_down += 1
 
-			count += 1
+        count += 1
 
-		return time_up - time_down
+        return time_up - time_down
 
-	def floor_position(self, floors):
-		value = floors
-		return self.calc_position(self, value)
+    def floor_position(self, floors):
+        value = floors
+        return self.calc_position(self, value)
 
-	def floor_position_input(self, floors):
-		value = open(floors).read()
-		return self.calc_position(self, value)
+    def floor_position_input(self, floors):
+        value = open(floors).read()
+        return self.calc_position(self, value)

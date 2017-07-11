08b40070b4b7a7831be605f91a375aba10e1b87c
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-07-11 16:16:06 -0300

    Update function calc_position in file floor.py

--- day1/floor.py
+++ day1/floor.py
@@ -1,18 +1,9 @@
 class Floor:
     @staticmethod
     def calc_position(value):
-        time_up = 0
-        time_down = 0
-        count = 0
-        while count < len(value):
-            if value[count] == '(':
-                time_up += 1
-            elif value[count] == ')':
-                time_down += 1
-
-            count += 1
-
-        return time_up - time_down
+        up = value.count('(')
+        down = value.count(')')
+        return up - down
 
     def floor_position(self, floors):
         value = floors

d81b15dd1603fdc3656cb97333975fa5a3462691
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-07-12 21:23:34 -0300

    Create file app.py to show result input.txt

---
+++ day1/app.py
@@ -0,0 +1,7 @@
+from day1.floor import Floor
+
+
+santa_current_floor = Floor().floor_position_input('test/fixtures/input.txt')
+
+print('To what floor do the instructions take Santa?')
+print(santa_current_floor)

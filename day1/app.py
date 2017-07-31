86455e50a67abea1262775a8fb2f57826854fcf6
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-07-31 10:37:54 -0300

    Remove variable file app.py

--- day1/app.py
+++ day1/app.py
@@ -2,7 +2,5 @@ from day1.floor import Floor
 
 
 input = open('input.txt').read()
-santa_current = Floor(input).santa_current_floor()
-
 print('To what floor do the instructions take Santa?')
-print(santa_current)
+print(Floor(input).santa_current_floor())

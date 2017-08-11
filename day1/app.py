632a0eb73b13e618a16faf9f5d4269b0eabf21b4
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-08-10 22:30:38 -0300

    Refactoring in the app.py file because of the changes in the santa.py file

--- day1/app.py
+++ day1/app.py
@@ -3,7 +3,7 @@ from day1.santa import Santa
 
 input = open('input.txt').read()
 print('To what floor do the instructions take Santa?')
-print(Santa(input).santa_current_floor)
+print(Santa(input).current_floor)
 print('What is the position of the character that causes Santa to first enter the basement?')
-print(Santa(input).santa_current_floor)
+print(Santa(input).first_enter_basement)
 

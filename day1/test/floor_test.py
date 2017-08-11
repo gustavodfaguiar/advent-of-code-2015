750197ee791c914c13cd7b65d72b0086c1845744
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-08-10 22:52:45 -0300

    Refactoring in the test/floor_test.py file because of the changes in the santa.py file

--- day1/test/floor_test.py
+++ day1/test/floor_test.py
@@ -1,19 +1,19 @@
 import unittest
-from day1.floor import Floor
+from day1.santa import Santa
 
 
 class FloorTest(unittest.TestCase):
     def test_up_floor(self):
-        self.assertEqual(Floor('(', 0).santa_current_floor, 1)
+        self.assertEqual(Santa('(').current_floor, 1)
 
     def test_down_floor(self):
-        self.assertEqual(Floor(')', 0).santa_current_floor, -1)
+        self.assertEqual(Santa(')').current_floor, -1)
 
     def test_up_down_floor(self):
-        self.assertEqual(Floor(')())())', 0).santa_current_floor, -3)
+        self.assertEqual(Santa(')())())').current_floor, -3)
 
     def test_up_down_floor_basement(self):
-        self.assertEqual(Floor('()())', -1).santa_current_floor, 5)
+        self.assertEqual(Santa('()())').first_enter_basement, 5)
 
 if __name__ == '__main__':
     unittest.main()

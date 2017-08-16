2a146d2d99f453f3eb8aade2fca100d582aca8ae
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-08-15 21:57:20 -0300

    Change method calculation_of_wrapping_paper

--- day2/app.py
+++ day2/app.py
@@ -2,11 +2,5 @@ from day2.gift import Gift
 
 
 dimensions = open("input.txt").readlines()
-dimensions = []
-
-for dimension in input:
-    dimension = dimension.strip()
-    dimensions.append(dimension)
-
 print('How many total square feet of wrapping paper should they order?')
 print(Gift().calculation_of_wrapping_paper(dimensions))

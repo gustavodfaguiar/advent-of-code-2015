6b52798933a8513a844b4eeedbddc4d103fdf231
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-08-15 21:57:20 -0300

    Change method calculation_of_wrapping_paper

--- day2/gift.py
+++ day2/gift.py
@@ -1,7 +1,12 @@
 class Gift:
     def calculation_of_wrapping_paper(self, dimensions):
         result = 0
-        for value in dimensions:
+        dimensions_new = []
+        for dimension in dimensions:
+            dimension = dimension.strip()
+            dimensions_new.append(dimension)
+
+        for value in dimensions_new:
             values_split = list(map(int, value.split('x')))
             small_value = min(values_split)
             result = result + (

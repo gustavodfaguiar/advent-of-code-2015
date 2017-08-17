99fea0ac271ea77b84f1eac5aa2993e9fb86932d
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-08-17 02:56:41 -0300

    Change function calculation wrapping paper and add test

--- day2/gift.py
+++ day2/gift.py
@@ -1,15 +1,22 @@
 class Gift:
     def calculation_of_wrapping_paper(self, dimensions):
-        result = 0
+        total = 0
         dimensions_new = []
         for dimension in list(dimensions):
             dimension = dimension.strip()
             dimensions_new.append(dimension)
 
         for dimension in dimensions_new:
+            result = []
             length, width, height = list(map(int, dimension.split('x')))
-            result += (
-                ((2 * length * width) +
-                 (2 * width * height) +
-                 (2 * height * length)))
-        return result
+
+            result.append(length * width)
+            result.append(width * height)
+            result.append(height * length)
+            result_min = min(result)
+
+            mult_result = lambda result: [value * 2 for value in result]
+            result = mult_result(result)
+            total += sum(result) + result_min
+
+        return total

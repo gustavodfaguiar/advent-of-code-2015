9701b214e445b681a100c2660879a6e8ea1e1325
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-09-21 17:34:09 -0300

    Revert "[WIP] Calculate the amount of wrapping paper for elves (#3)"
    This reverts commit def2f8c5a506654ad54080ff4ac21ad747c6b773.

--- advent_of_code/day2/app.py
+++
@@ -1,9 +0,0 @@
-from advent_of_code.day2.gift import Gift
-
-
-dimensions = open("input.txt").readlines()
-gift = Gift(dimensions)
-print('How many total square feet of wrapping paper should they order?')
-print(gift.calculation_of_wrapping_paper())
-print('How many total feet of ribbon should they order?')
-print(gift.calculation_of_feet_ribbon())
--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ from distutils.core import setup
 setup(name='advent_of_code',
       version='0.1',
       description='Advent of code',
-      packages=['advent_of_code'])
+      packages=['day1'])

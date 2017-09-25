782cafdd0ca6a091af06b84ed37ad47afc34d147
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-09-25 20:17:35 -0300

    Defining responsibilities

--- advent_of_code/day2/gift.py
+++ advent_of_code/day2/gift.py
@@ -1,33 +1,36 @@
 class Gift:
-    def __init__(self, dimensions):
-        self.dimensions = dimensions
+	def __init__(self, dimensions):
+		self.dimensions = dimensions
 
-    def calculation_of_wrapping_paper(self):
-        total = 0
+	def calculation_of_wrapping_paper(self):
+		total = 0
 
-        for dimension in self.dimensions:
-            try:
-                length, width, height = list(map(int, dimension.split('x')))
-            except Exception:
-                raise Exception('Missing parameters')
- 
-            first = length * width
-            second = width * height
-            third = height * length
+		for dimension in self.dimensions:
+			try:
+				length, width, height = self.get_sizes_of(dimension)
+			except Exception:
+				raise Exception('Missing parameters')
 
-            slack = min([first, second, third])
-            paper = sum([first, second, third]) * 2
+			first = length * width
+			second = width * height
+			third = height * length
 
-            total += paper + slack
+			slack = min([first, second, third])
+			paper = sum([first, second, third]) * 2
 
-        return total
+			total += paper + slack
 
-    def calculation_of_feet_ribbon(self):
-        total = 0
+		return total
 
-        for dimension in self.dimensions:
-            length, width, height = list(map(int, dimension.split('x')))
-            min_value = min([length + width, width + height, height + length]) * 2
-            total += min_value + width * height * length
+	def get_sizes_of(self, dimension):
+		return list(map(int, dimension.split('x')))
 
-        return total
+	def calculation_of_feet_ribbon(self):
+		total = 0
+
+		for dimension in self.dimensions:
+			length, width, height = list(map(int, dimension.split('x')))
+			min_value = min([length + width, width + height, height + length]) * 2
+			total += min_value + width * height * length
+
+		return total

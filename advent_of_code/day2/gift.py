974d31da4a0c8e6ff47dc2cabc0b433f0c8d2072
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-09-27 15:20:33 -0300

    Corrigindo merge

--- advent_of_code/day2/gift.py
+++ advent_of_code/day2/gift.py
@@ -1,38 +1,4 @@
 class Gift:
-<<<<<<< HEAD
-    def __init__(self, dimensions):
-        self.dimensions = dimensions
-
-    def calculation_of_wrapping_paper(self):
-        total = 0
-
-        for dimension in self.dimensions:
-            try:
-                length, width, height = list(map(int, dimension.split('x')))
-            except Exception:
-                raise Exception('Missing parameters')
- 
-            first = length * width
-            second = width * height
-            third = height * length
-
-            slack = min([first, second, third])
-            paper = sum([first, second, third]) * 2
-
-            total += paper + slack
-
-        return total
-
-    def calculation_of_feet_ribbon(self):
-        total = 0
-
-        for dimension in self.dimensions:
-            length, width, height = list(map(int, dimension.split('x')))
-            min_value = min([length + width, width + height, height + length]) * 2
-            total += min_value + width * height * length
-
-        return total
-=======
 	def __init__(self, dimensions):
 		self.dimensions = dimensions
 
@@ -71,4 +37,3 @@ class Gift:
 			total += min_value + width * height * length
 
 		return total
->>>>>>> a4ed3a23d3e1d2404d1a40d2ee34f10b33bbe876
